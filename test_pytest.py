5e441d78b246ecd323045a840ee1cf8c3991a493
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-08-09 17:22:05 +0200

    ...pytest study...

--- test_pytest.py
+++ test_pytest.py
@@ -1,7 +1,50 @@
-def test_if_it_works():
-	assert 1==1
+import pytest
 
-def test_if_it_doesnt_work():
+@pytest.fixture(scope='function')
+def functionFixture(request):
+	print('\nSetting "functionFixture" up')
+	def finalizer():
+		print('\nFinalizing "functionFixture"')
+	request.addfinalizer(finalizer)
+	return 1
+
+@pytest.fixture(scope='module')
+def moduleFixture(request):
+	print('\nSetting "moduleFixture" up')
+	def finalizer():
+		print('\nFinalizing "moduleFixture"')
+	request.addfinalizer(finalizer)
+	return 10
+
+@pytest.fixture(scope='module')
+def yield_fixture():
+	print('\nSetting "yield_fixture" up. scope=module')
+	yieldedValue = 3.14
+	yield yieldedValue
+	print('\nFinalizing "yield_fixture"')
+
+@pytest.mark.basic
+def test_assert(moduleFixture):
+	print('\nassert test using moduleFixture')
+	assert moduleFixture == 10
+
+@pytest.mark.mid_level
+def test_list(moduleFixture):
+	print('\nlist test using moduleFixture')
 	a = [1, 2, 3]
-	b = (3, -2, 5)
-	assert a==b
+	b = [10, 20, 30]
+	assert [moduleFixture*x for x in a] == b
+
+@pytest.mark.with_yield
+def test_yield_fixture(yield_fixture):
+	assert yield_fixture == 3.14
+
+def square(x):
+	return x*x
+
+@pytest.mark.advanced
+@pytest.mark.parametrize('input, expected', [(2,4), (3,9), (5,25)])
+def test_parametrized(functionFixture, input, expected):
+	print('\nparametrized test using functionFixture')
+	assert expected == input**2
+	assert functionFixture == 1
